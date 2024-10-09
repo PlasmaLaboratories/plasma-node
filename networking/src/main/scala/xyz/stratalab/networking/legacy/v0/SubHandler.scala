81a6f697c9545efac5b8ed9e64293aed26efa4f8
Fernando Torterolo <nandotorterolo@gmail.com>
2024-10-09 10:55:45 -0300

    BN-1550-remove-legacy-network-protocol

--- networking/src/main/scala/xyz/stratalab/networking/legacy/v0/SubHandler.scala
+++
@@ -1,8 +0,0 @@
-package xyz.stratalab.networking.legacy
-
-import fs2._
-
-/**
- * Consumes and produces messages for a particular session inside of a multiplexer
- */
-case class SubHandler[F[_]](sessionId: Byte, subscriber: Pipe[F, Chunk[Byte], Unit], producer: Stream[F, Chunk[Byte]])
