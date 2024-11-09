ae6a50e2998066ab66d2cbca11c0829a4d80668f
Scala Steward <scala_steward@virtuslab.com>
2024-11-09 20:16:15 +0000

    Update circe-core to 0.14.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val circeVersion = "0.14.7"
+  val circeVersion = "0.14.10"
   val kamonVersion = "2.7.2"
   val catsCoreVersion = "2.10.0"
   val catsEffectVersion = "3.5.4"
