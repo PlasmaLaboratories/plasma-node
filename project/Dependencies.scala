109da18523e5017bf8fec6b54cd68787ec5eb299
Scala Steward <scala_steward@virtuslab.com>
2024-11-09 20:19:45 +0000

    Update cats-core to 2.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val circeVersion = "0.14.10"
   val kamonVersion = "2.7.5"
-  val catsCoreVersion = "2.10.0"
+  val catsCoreVersion = "2.12.0"
   val catsEffectVersion = "3.5.4"
   val fs2Version = "3.11.0"
   val logback = "1.5.12"
