f1da19eef35d2d3bafdef9adbdff706459b2c775
Fernando <ft@plasma.to>
2024-11-11 14:30:00 -0300

    Update Dependencies.scala (#33)
    Update protobuf-spec dependencies to  'v0.1.2'

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   val orientDbVersion = "3.2.34"
   val ioGrpcVersion = "1.64.0"
   val http4sVersion = "0.23.26"
-  val protobufSpecsVersion = "0.1.1+2-290af250-SNAPSHOT"
+  val protobufSpecsVersion = "0.1.2"
   val plasmaSdkVersion = "0.2.0"
   val catsSlf4j =
     "org.typelevel" %% "log4cats-slf4j" % "2.7.0"
