f2d366bb1bba4d7e88845ec5397528f9dff366f4
Fernando Torterolo <nandotorterolo@gmail.com>
2024-11-13 17:16:45 -0300

    Revert "update scala, scoverage and iogrpc"
    This reverts commit e3b2b5579c4a842e1c498188424777060a927f11.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val fs2Version = "3.11.0"
   val logback = "1.5.12"
   val orientDbVersion = "3.2.35"
-  val ioGrpcVersion = "1.68.1"
+  val ioGrpcVersion = "1.64.2"
   val http4sVersion = "0.23.29"
   val protobufSpecsVersion = "0.1.2"
   val plasmaSdkVersion = "0.2.0"
@@ -53,7 +53,8 @@ object Dependencies {
   )
 
   val cats: Seq[ModuleID] = Seq(
-    "org.typelevel" %% "cats-core" % catsCoreVersion
+    "org.typelevel" %% "cats-core" % catsCoreVersion,
+    "org.typelevel" %% "mouse"     % "1.2.4"
   )
 
   val catsEffect: Seq[ModuleID] = Seq(
