50f1231d94264f1dd071e02bf5d788cf7e01bebe
Scala Steward <scala_steward@virtuslab.com>
2024-11-13 18:25:09 +0000

    Update mouse to 1.3.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -54,7 +54,7 @@ object Dependencies {
 
   val cats: Seq[ModuleID] = Seq(
     "org.typelevel" %% "cats-core" % catsCoreVersion,
-    "org.typelevel" %% "mouse"     % "1.2.4"
+    "org.typelevel" %% "mouse"     % "1.3.2"
   )
 
   val catsEffect: Seq[ModuleID] = Seq(
